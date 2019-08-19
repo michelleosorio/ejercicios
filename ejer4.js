39a1f967c379629bef171d2cf38137060ffcce50
michelleosorio <stefany_osorioca@fet.edu.co>
2019-08-18 20:11:43 -0500

    ejercicios
    ejercicios de node j.s

---
+++ ejer4.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+
+var app = express();
+
+
+app.get('/', function(req, res){
+  res.sendfile(__dirname + '/views/ejer4.html');
+});
+app.listen(3000);
+console.log("Servidor Express escuchando en modo %s", app.settings.env);
